a896b17e60b5c47d3bf4504039377548cd97013a
Trojanez <trojanez758@gmail.com>
2018-04-02 01:01:26 +0300

    Registration php OOP

---
+++ register.php
@@ -0,0 +1,33 @@
+<?php 
+
+	class Register extends User {
+		public $confirm_password;
+		public $errors = [];
+
+		public function __construct($login, $password, $confirm_password, $email) {
+			$this->login = $login;
+			$this->password = $password;
+			$this->confirm_password = $confirm_password;
+			$this->email = $email;
+		}
+
+		public function unique(array $row, $err) {
+			if(array_shift($row)) {
+				$this->errors[] = $err;
+			}
+		}
+
+		public function checkPassword($pass1, $pass2, $err) {
+			if($pass1 != $pass2) {
+				$this->errors[] = $err;
+			}
+		}
+
+		public function passHash($algo = PASSWORD_DEFAULT, array $options = null) {
+			$this->password = password_hash($this->password, $algo);
+		}
+
+		public function getErrors() {
+			return $this->errors;
+		}
+	}
