31d52c5f7b211fd3f169aec943cfbcaf15c8f327
user <faust1234a@gmail.com>
2018-04-04 22:38:53 +0300

    Del

--- Release/2.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE `user` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL DEFAULT '',
-  `surname` varchar(30) NOT NULL DEFAULT '',
-  `img` varchar(50) NOT NULL DEFAULT '',
-  `city` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-);
